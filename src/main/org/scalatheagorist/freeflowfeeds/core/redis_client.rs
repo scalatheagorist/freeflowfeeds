5ebd602d44fea4c785370312b5ab273fc2399e7e
scalanakamoto <lightningrises@proton.me>
2023-10-09 16:08:41 +0200

    distinct mechanism for redis

--- src/main/org/scalatheagorist/freeflowfeeds/core/redis_client.rs
+++ src/main/org/scalatheagorist/freeflowfeeds/core/redis_client.rs
@@ -3,6 +3,7 @@ use std::vec::IntoIter;
 use futures_util::StreamExt;
 use log::{error, info, warn};
 use map_for::FlatMap;
+use num_traits::Zero;
 use redis::Commands;
 use serde::{Deserialize, Serialize};
 use tokio_stream::Iter;
@@ -18,9 +19,9 @@ pub struct RedisConfig {
 pub struct RedisClient;
 
 impl RedisClient {
-    pub async fn rpush(
+    pub async fn rpush_distinct(
         config: &RedisConfig,
-        channel: &str,
+        key: &str,
         values: Iter<IntoIter<RSSFeed>>,
     ) {
         values.for_each(|value| {
@@ -29,21 +30,22 @@ impl RedisClient {
                 redis::Client::open(addr.clone())
                     .map_err(|err| error!("could not create redis client: {}", err))
                     .ok()
-                    .flat_map(|c| {
-                        c.get_connection()
+                    .flat_map(|client| {
+                        client
+                            .get_connection()
                             .map_err(|err| error!("could not connect to redis: {}", err))
                             .ok()
                     })
                     .flat_map(|mut conn| {
-                        match redis::cmd("RPUSH")
-                            .arg(channel)
-                            .arg(&*value.clone().writes().unwrap())
-                            .query::<i32>(&mut conn) {
-                            Ok(_) => Some(()),
-                            Err(err) => {
-                                warn!("redis rpush fail {}", err);
-                                None
-                            }
+                        match redis::cmd("LPOS").arg(key).arg(&*value.clone().writes().unwrap()).query::<i32>(&mut conn) {
+                            Ok(return_key) if return_key.is_positive() || return_key.is_zero() => None,
+                            _ =>
+                                conn.rpush::<&str, &str, i32>(key, &*value.clone().writes().unwrap())
+                                    .map_err(|err| {
+                                        warn!("redis RPUSH failed: {}", err)
+                                    })
+                                    .ok()
+                                    .map(|_| ())
                         }
                     })
                     .unwrap_or_else(|| ());
@@ -58,21 +60,22 @@ impl RedisClient {
         channels: String,
     ) -> Iter<IntoIter<String>> {
         let addr: String = format!("redis://{}/", config.node_address);
-        let iter_maybe =
+        let iter_maybe: Option<Iter<IntoIter<String>>> =
             redis::Client::open(addr.clone())
                 .map_err(|err| error!("could not create redis client: {}", err))
                 .ok()
-                .flat_map(|c| {
-                    c.get_connection()
+                .flat_map(|client| {
+                    client
+                        .get_connection()
                         .map_err(|err| error!("could not connect to redis: {}", err))
                         .ok()
                 })
-                .flat_map(|mut con| {
+                .flat_map(|mut conn| {
                     info!("client subscription connected: {}", addr);
-                    match con.lrange::<&str, Vec<String>>(&*channels, 0, -1) {
+                    match conn.lrange::<&str, Vec<String>>(&*channels, 0, -1) {
                         Ok(vec) => Some(tokio_stream::iter(vec)),
                         Err(err) => {
-                            warn!("redis rpush fail {}", err);
+                            warn!("redis LRANGE failed: {}", err);
                             None
                         }
                     }
