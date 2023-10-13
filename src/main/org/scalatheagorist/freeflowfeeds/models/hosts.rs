8b165d9bb4815f73577462f7774bcb2bcc7b8b01
scalanakamoto <lightningrises@proton.me>
2023-10-14 00:15:16 +0200

    sorted search by pages ascendancy

--- src/main/org/scalatheagorist/freeflowfeeds/models/hosts.rs
+++ src/main/org/scalatheagorist/freeflowfeeds/models/hosts.rs
@@ -11,15 +11,36 @@ pub struct Hosts {
 
 impl Hosts {
     pub fn as_publisher(&self) -> Vec<(Publisher, String)> {
-        [
+        let mut publishers: Vec<(Publisher, String)> =
             vec![
                 (Publisher::EFMAGAZIN, self.efmagazin.url.clone()),
                 (Publisher::FREIHEITSFUNKEN, self.freiheitsfunken.url.clone()),
-                (Publisher::MISESDE, self.misesde.url.clone())],
+                (Publisher::MISESDE, self.misesde.url.clone())
+            ];
+
+        let mut publishers_with_pages = vec![
             (self.efmagazin.with_pages()),
             (self.misesde.with_pages()),
             (self.freiheitsfunken.with_pages())
-        ]
-            .concat()
+        ].concat();
+
+        publishers_with_pages.sort_by(|a, b| {
+            let num_a = a.1
+                .rsplit('=')
+                .next()
+                .and_then(|num| num.parse::<u32>().ok())
+                .unwrap_or(u32::MAX);
+            let num_b = b.1
+                .rsplit('=')
+                .next()
+                .and_then(|num| num.parse::<u32>().ok())
+                .unwrap_or(u32::MAX);
+
+            num_a.cmp(&num_b)
+        });
+
+        [
+            publishers, publishers_with_pages
+        ].concat()
     }
 }
