e803f432b02f6d70ea114db744d18a9d87f581fb
scalanakamoto <lightningrises@proton.me>
2024-01-27 16:49:29 +0100

    donation

--- src/main/org/scalatheagorist/freeflowfeeds/frontend/view/index_html.rs
+++ src/main/org/scalatheagorist/freeflowfeeds/frontend/view/index_html.rs
@@ -351,6 +351,30 @@ body {
                     </div>
                 </div>
             </li>
+            <li class="nav-item text-center mr-auto">
+                <button type="button" class="btn btn-secondary nav-btn" data-toggle="modal" data-target="#donateModal">
+                    Donate
+                </button>
+                <div class="modal fade" id="donateModal" tabindex="-1" role="dialog" aria-labelledby="donateModal" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="donateModalLabel">Donate</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body text-left">
+                                <p>lightning donation: scalatheagorist@blink.sv</p>
+                                <img src="https://image.nostr.build/b4fdbe9b9eb6798c44e68b0ff391065d78ebb9fddad9a151cea2c8de37bd8041.jpg">
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
         </ul>
     </div>
 </nav>
